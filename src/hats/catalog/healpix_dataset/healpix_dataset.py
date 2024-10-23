astronomy-commons/hipscat

use plot_pixels method in catalog

src/hipscat/catalog/healpix_dataset/healpix_dataset.py
@@ -16,7 +16,7 @@ import hipscat.pixel_math.healpix_shim as hp
 from hipscat.catalog.catalog_info import CatalogInfo
 from hipscat.catalog.dataset import Dataset
 from hipscat.catalog.partition_info import PartitionInfo
-from hipscat.inspection import plot_pixel_list
+from hipscat.inspection import plot_pixel_list, plot_pixels
 from hipscat.io import file_io, paths
 from hipscat.io.file_io import read_parquet_metadata
 from hipscat.pixel_math import HealpixPixel
@@ -232,9 +232,4 @@ class HealpixDataset(Dataset):
         )
 
     def plot_pixels(self, projection: str = "MOL", **kwargs):
-        return plot_pixel_list(
-            self.get_healpix_pixels(),
-            f"HATS HEALPix structure of {self.catalog_name}",
-            projection,
-            **kwargs,
-        )
+        return plot_pixels(self, projection, **kwargs)

src/hipscat/inspection/visualize_catalog.py
@@ -31,6 +31,7 @@ from hipscat.pixel_math import HealpixPixel
 
 if TYPE_CHECKING:
     from hipscat.catalog import Catalog
+    from hipscat.catalog.healpix_dataset.healpix_dataset import HealpixDataset
 
 
 def _read_point_map(catalog_base_dir):
@@ -65,7 +66,7 @@ def plot_points(catalog: Catalog, projection="MOL", **kwargs):
     )
 
 
-def plot_pixels(catalog: Catalog, projection="MOL", **kwargs):
+def plot_pixels(catalog: HealpixDataset, projection="MOL", **kwargs):
     """Create a visual map of the pixel density of the catalog.
 
     Args:

astronomy-commons/hats

src/hats/catalog/healpix_dataset/healpix_dataset.py
@@ -15,7 +15,7 @@ import hats.pixel_math.healpix_shim as hp
 from hats.catalog.dataset import Dataset
 from hats.catalog.dataset.table_properties import TableProperties
 from hats.catalog.partition_info import PartitionInfo
-from hats.inspection import plot_pixel_list
+from hats.inspection import plot_pixel_list, plot_pixels
 from hats.io import file_io, paths
 from hats.io.file_io import read_parquet_metadata
 from hats.pixel_math import HealpixPixel
@@ -228,9 +228,4 @@ class HealpixDataset(Dataset):
         )
 
     def plot_pixels(self, projection: str = "MOL", **kwargs):
-        return plot_pixel_list(
-            self.get_healpix_pixels(),
-            f"HATS HEALPix structure of {self.catalog_name}",
-            projection,
-            **kwargs,
-        )
+        return plot_pixels(self, projection, **kwargs)

src/hats/inspection/visualize_catalog.py
@@ -32,6 +32,7 @@ from hats.pixel_math import HealpixPixel
 
 if TYPE_CHECKING:
     from hats.catalog import Catalog
+    from hats.catalog.healpix_dataset.healpix_dataset import HealpixDataset
 
 
 def _read_point_map(catalog_base_dir):
@@ -66,7 +67,7 @@ def plot_points(catalog: Catalog, projection="MOL", **kwargs):
     )
 
 
-def plot_pixels(catalog: Catalog, projection="MOL", **kwargs):
+def plot_pixels(catalog: HealpixDataset, projection="MOL", **kwargs):
     """Create a visual map of the pixel density of the catalog.
 
     Args: