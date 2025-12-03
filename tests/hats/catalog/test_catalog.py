98ebfeb6d72635c5cd3d00950709def2ff03546f
Sean McGuire <123987820+smcguire-cmu@users.noreply.github.com>
2025-12-03 13:49:53 -0500

    Update tests/hats/catalog/test_catalog.py
    Co-authored-by: Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>

--- tests/hats/catalog/test_catalog.py
+++ tests/hats/catalog/test_catalog.py
@@ -119,6 +119,7 @@ def test_catalog_statistics(small_sky_order1_dir):
     with pytest.warns(UserWarning, match="modified catalog"):
         result_frame = filtered_catalog.aggregate_column_statistics()
     assert len(result_frame) == 5
+    assert_column_stat_as_floats(result_frame, "dec", min_value=-69.5, max_value=-47.5, row_count=42)
 
     result_frame = cat.per_pixel_statistics()
     # 4 = 4 pixels
