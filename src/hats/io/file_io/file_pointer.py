7d690d81a91fa0d0139ba7650726f4be95b99ca8
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2026-09-09 14:39:54 -0400

    Use additional storage options for collection paths.

--- src/hats/io/file_io/file_pointer.py
+++ src/hats/io/file_io/file_pointer.py
@@ -10,13 +10,15 @@ BLOCK_SIZE = 32 * 1024
 _VIZCAT_HOST = "vizcat.cds.unistra.fr"
 
 
-def get_upath(path: str | Path | UPath) -> UPath:
+def get_upath(path: str | Path | UPath, **kwargs) -> UPath:
     """Returns a UPath file pointer from a path string or other path-like type.
 
     Parameters
     ----------
     path: str | Path | UPath
         base file path to be normalized to UPath
+    **kwargs
+        additional storage options required for the file system backend.
 
     Returns
     -------
@@ -27,10 +29,10 @@ def get_upath(path: str | Path | UPath) -> UPath:
         return None
     if isinstance(path, UPath):
         return path
-    return get_upath_for_protocol(path)
+    return get_upath_for_protocol(path, **kwargs)
 
 
-def get_upath_for_protocol(path: str | Path) -> UPath:
+def get_upath_for_protocol(path: str | Path, **kwargs) -> UPath:
     """Create UPath with protocol-specific configurations.
 
     If we access pointers on S3 and credentials are not found we assume
@@ -40,17 +42,23 @@ def get_upath_for_protocol(path: str | Path) -> UPath:
     ----------
     path: str | Path | UPath
         base file path to be normalized to UPath
+    **kwargs
+        additional storage options required for the file system backend.
 
     Returns
     -------
     UPath
         Instance of UPath.
     """
-    upath = UPath(path)
+    upath = UPath(path, **kwargs)
     if upath.protocol == "s3":
-        upath = UPath(path, anon=True, default_block_size=BLOCK_SIZE)
-    if upath.protocol in ("http", "https"):
         kwargs = {
+            "anon": True,
+            "default_block_size": BLOCK_SIZE,
+        } | kwargs
+        upath = UPath(path, **kwargs)
+    if upath.protocol in ("http", "https"):
+        kwargs = kwargs | {
             "block_size": BLOCK_SIZE,
             "client_kwargs": {"headers": {"User-Agent": f"hats/{version('hats')}"}},
         }
