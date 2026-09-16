4de1854326e6ab718f91786984539b71f433aff7
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2026-09-16 12:32:18 -0400

    Allow unexpected keys, and use approx comparison for properties. (#746)
    * Allow unexpected keys, and use approx comparison for properties.
    
    * Ignore expected warnings.
    
    * Don't worry about testing.

--- src/hats/testing/helpers.py
+++ src/hats/testing/helpers.py
@@ -1,3 +1,17 @@
+# pylint: disable=import-outside-toplevel,import-error
+def _dict_almost_equal(dict_expected, dict_test):  # pragma: no cover
+    try:
+        import pytest
+    except ImportError as exc:
+        raise ImportError("pytest is required to use this method. Install with pip or conda.") from exc
+
+    assert dict_expected.keys() == dict_test.keys()
+    for key in dict_expected:
+        assert dict_expected[key] == pytest.approx(
+            dict_test[key], rel=1e-4
+        ), f"Failed approx comparison for {key} (expected: {dict_expected[key]}, test: {dict_test[key]})"
+
+
 def assert_catalog_info_is_correct(
     expected_catalog_info,
     catalog_info,
@@ -5,7 +19,7 @@ def assert_catalog_info_is_correct(
     do_not_compare: list[str] | None = None,
     check_extra_properties: bool = True,
     **properties_to_update,
-):
+):  # pragma: no cover
     """Check that the catalog properties are similar to the expected ones."""
     if do_not_compare is None:
         do_not_compare = []
@@ -13,6 +27,6 @@ def assert_catalog_info_is_correct(
     do_not_compare_dict = {prop: None for prop in do_not_compare}
     expected_catalog_info = expected_catalog_info.copy_and_update(**do_not_compare_dict)
     catalog_info = catalog_info.copy_and_update(**(properties_to_update | do_not_compare_dict))
-    assert expected_catalog_info.explicit_dict() == catalog_info.explicit_dict()
+    _dict_almost_equal(expected_catalog_info.explicit_dict(), catalog_info.explicit_dict())
     if check_extra_properties:
-        assert expected_catalog_info.extra_dict() == catalog_info.extra_dict()
+        _dict_almost_equal(expected_catalog_info.extra_dict(), catalog_info.extra_dict())
